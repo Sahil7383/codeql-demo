b71b1a886b86fa4ae8adac45d20005f79db72704
Sahil Naliyadhara <sahilnaliyadhara110@gmail.com>
2026-06-16 19:38:59 +0530

    changes in server.js file

--- server.js
+++ server.js
@@ -1,33 +1,14 @@
-const express = require("express");
-const { exec } = require("child_process");
+const express = require('express');
+const { exec } = require('child_process');
 
 const app = express();
 
-const rateLimit = require("express-rate-limit");
+app.get('/ping', (req, res) => {
+    const host = req.query.host;
 
-const runLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 20
-});
-
-app.get("/run", runLimiter, (req, res) => {
-
-    const command = req.query.cmd;
-
-    const allowedCommands = {
-        date: "date",
-        uptime: "uptime"
-    };
-
-    const cmd = allowedCommands[req.query.cmd];
-
-    if (!cmd) {
-        return res.status(400).send("Invalid command");
-    }
-
-    exec(cmd);
-
-    res.send("Executed");
+    exec(`ping ${host}`, (err, stdout) => {
+        res.send(stdout);
+    });
 });
 
 app.listen(3000);
